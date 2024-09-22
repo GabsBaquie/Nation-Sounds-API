f7347a8dc26b509328508a84cd0bbcf2f2f8de97
GabsBaquie <117845004+GabsBaquie@users.noreply.github.com>
2024-09-22 18:56:49 +0200

    Refactor console log message to correctly display the server port

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ AppDataSource.initialize()
   .then(() => {
     const PORT = process.env.PORT || 4000;
     app.listen(PORT, () => {
-      console.log(`Serveur démarré sur le port http://localhost:3000${PORT}`);
+      console.log(`Serveur démarré sur le port http://localhost:${PORT}`);
     });
   })
   .catch((error) => console.log(error));
