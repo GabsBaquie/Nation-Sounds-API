f0a232cd63e530e7beb059f04e153d3f2c445d01
GabsBaquie <117845004+GabsBaquie@users.noreply.github.com>
2025-01-19 16:12:14 +0100

    Fix compilation and deployment issues

--- src/types/typeorm.d.ts
+++ src/types/typeorm.d.ts
@@ -1,9 +1,1 @@
-declare global {
-    namespace TypeORM {
-        interface Connection {
-        getRepository<T>(entity: string): Repository<T>;
-        }
-    }
-    }
-
-export {};
+declare module 'typeorm';
